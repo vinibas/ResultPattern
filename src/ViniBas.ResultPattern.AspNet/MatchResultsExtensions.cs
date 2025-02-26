23eeb037cd06690a2fc70d7138d85df81256ff1d
Vinícius Bastos da Silva <viny.bas@gmail.com>
2025-02-25 22:39:52 -0300

    Added documentation to Match methods. Testing tweaks: separated Match and ProblemDetails extensions, and created missing tests for these methods.

--- src/ViniBas.ResultPattern.AspNet/MatchResultsExtensions.cs
+++ src/ViniBas.ResultPattern.AspNet/MatchResultsExtensions.cs
@@ -6,26 +6,59 @@ namespace ViniBas.ResultPattern.AspNet;
 
 public static class MatchResultsExtensions
 {
+    /// <summary>
+    /// Checks whether a ResultResponse is a success or failure, and returns the result of the function if successful.
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <returns>Returns the result of the onSuccess function on success, or a ProblemDetails on failure.</returns>
     public static IActionResult Match(this ResultResponse resultResponse,
         Func<ResultResponse, IActionResult> onSuccess)
         => resultResponse.Match(onSuccess, response => response.ToProblemDetailsActionResult());
 
+    /// <summary>
+    /// Checks whether a ResultResponse is a success or failure, and returns the result of the corresponding function
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <param name="onFailure">Function to be executed in case of failure</param>
+    /// <returns>Returns the result of the onSuccess function in case of success, or of onFailure in case of failure.</returns>
     public static IActionResult Match(this ResultResponse resultResponse,
         Func<ResultResponse, IActionResult> onSuccess, Func<ResultResponse, IActionResult> onFailure)
         => resultResponse.IsSuccess ? onSuccess(resultResponse) : onFailure(resultResponse);
     
+    /// <summary>
+    /// Checks whether a Result is a success or failure, and returns the result of the function if successful.
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <returns>Returns the result of the onSuccess function on success, or a ProblemDetails on failure.</returns>
     public static IActionResult Match(this Result result,
         Func<ResultResponse, IActionResult> onSuccess)
         => result.ToActionResponse().Match(onSuccess);
 
+    /// <summary>
+    /// Checks whether a Result is a success or failure, and returns the result of the corresponding function
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <param name="onFailure">Function to be executed in case of failure</param>
+    /// <returns>Returns the result of the onSuccess function in case of success, or of onFailure in case of failure.</returns>
     public static IActionResult Match(this Result result,
         Func<ResultResponse, IActionResult> onSuccess, Func<ResultResponse, IActionResult> onFailure)
         => result.ToActionResponse().Match(onSuccess, onFailure);
     
+    /// <summary>
+    /// Checks whether a Result<T> is a success or failure, and returns the result of the function if successful.
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <returns>Returns the result of the onSuccess function on success, or a ProblemDetails on failure.</returns>
     public static IActionResult Match<T>(this Result<T> result,
         Func<ResultResponse, IActionResult> onSuccess)
         => result.ToActionResponse().Match(onSuccess);
 
+    /// <summary>
+    /// Checks whether a Result<T> is a success or failure, and returns the result of the corresponding function
+    /// </summary>
+    /// <param name="onSuccess">Function to be executed in case of success.</param>
+    /// <param name="onFailure">Function to be executed in case of failure</param>
+    /// <returns>Returns the result of the onSuccess function in case of success, or of onFailure in case of failure.</returns>
     public static IActionResult Match<T>(this Result<T> result,
         Func<ResultResponse, IActionResult> onSuccess, Func<ResultResponse, IActionResult> onFailure)
         => result.ToActionResponse().Match(onSuccess, onFailure);
--- tests/ViniBas.ResultPattern.AspNet.UnitTests/ProblemDetailsExtensionsTests.cs
+++ tests/ViniBas.ResultPattern.AspNet.UnitTests/ProblemDetailsExtensionsTests.cs
@@ -5,53 +5,10 @@ using ViniBas.ResultPattern.ResultObjects;
 
 namespace ViniBas.ResultPattern.AspNet.UnitTests;
 
-public class ResultExtensionsTests
+public class ProblemDetailsExtensionsTests
 {
     private readonly ResultResponseError _resultResponse = new ([ "Error 1", "Error 2" ], ErrorType.Validation);
     private readonly string _problemDetailsDetail = string.Join(Environment.NewLine, [ "Error 1", "Error 2" ]);
-
-    [Fact]
-    public void Match_ShouldReturnOnSuccess()
-    {
-        var result = new ResultResponseSuccess().Match(_ => new CreatedResult());
-        var resultData = new ResultResponseSuccess<string>("Test Data").Match(rr => new OkObjectResult(rr));
-
-        Assert.IsType<CreatedResult>(result);
-
-        var okResult = Assert.IsType<OkObjectResult>(resultData);
-        var resultResponseValue = Assert.IsType<ResultResponseSuccess<string>>(okResult.Value);
-        Assert.True(resultResponseValue.IsSuccess);
-        Assert.Equal("Test Data", resultResponseValue.Data);
-    }
-
-    [Fact]
-    public void Match_ShouldReturnOnFailure()
-    {
-        Func<ResultResponse, IActionResult> onSuccess = response => new NoContentResult();
-        Func<ResultResponse, IActionResult> onSuccessData = response => new ObjectResult("Test Data");
-        Func<ResultResponse, IActionResult> onFailure = response => new BadRequestResult();
-
-        var result = _resultResponse.Match(onSuccess, onFailure);
-        var resultData = _resultResponse.Match(onSuccessData, onFailure);
-
-        Assert.IsType<BadRequestResult>(result);
-        Assert.IsType<BadRequestResult>(resultData);
-    }
-
-    [Fact]
-    public void Match_ShouldReturnProblemDetailsOnFailure()
-    {
-        var result = _resultResponse.Match(_ => new NoContentResult());
-        var resultData = _resultResponse.Match(_ => new ObjectResult("Test Data"));
-
-        var objRes = Assert.IsType<ObjectResult>(result);
-        var probDet = Assert.IsType<ProblemDetails>(objRes.Value);
-        Assert.Equal(_problemDetailsDetail, probDet.Detail);
-        
-        objRes = Assert.IsType<ObjectResult>(resultData);
-        probDet = Assert.IsType<ProblemDetails>(objRes.Value);
-        Assert.Equal(_problemDetailsDetail, probDet.Detail);
-    }
 
     [Fact]
     public void ToProblemDetails_ShouldReturnProblemDetails()
