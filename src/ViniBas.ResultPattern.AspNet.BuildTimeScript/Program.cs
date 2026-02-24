2bad65963da3caa2fc105560ca8f46ff9d59f970
Vinícius Bastos da Silva <viny.bas@gmail.com>
2026-02-24 11:54:40 -0300

    refactor: Created the automatic generation project that uses the BuildTime Script strategy.

---
+++ src/ViniBas.ResultPattern.AspNet.BuildTimeScript/Program.cs
@@ -0,0 +1,15 @@
+﻿using ViniBas.ResultPattern.AspNet.BuildTimeScript.Builders;
+
+var outputPath = args[0];
+Directory.CreateDirectory(outputPath);
+Console.WriteLine($"Generating code in: {outputPath}");
+
+var variationsGenerator = new VariationsGenerator();
+
+var mvcClass = variationsGenerator.GenerateMvcClass();
+var minimalApiClass = variationsGenerator.GenerateMinimalApiClass();
+var minimalApiClassTyped = variationsGenerator.GenerateMinimalApiTypedClass();
+
+File.WriteAllText(Path.Combine(outputPath, "MatchResultsExtensions_Mvc.g.cs"), mvcClass);
+File.WriteAllText(Path.Combine(outputPath, "MatchResultsExtensions_MinimalApi.g.cs"), minimalApiClass);
+File.WriteAllText(Path.Combine(outputPath, "MatchResultsExtensions_MinimalApi_Typed.g.cs"), minimalApiClassTyped);
